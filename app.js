97f2e8b7c7cccace43ba8f4634c94d2ac299e404
HendrikBD <hendrik.danen@gmail.com>
2018-05-23 11:53:49 -0400

    Changing email redirect to app.js

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ app.set('view engine', 'ejs');
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', indexRoutes);
-
-app.get("/email", function(req, res){
-
+app.get("/index.php", function(req, res){
+  
   var name = req.query.name,
     email = req.query.email,
     phone = req.query.phone,
@@ -30,6 +28,25 @@ app.get("/email", function(req, res){
 
   res.render("reload");
 })
+
+app.use('/', indexRoutes);
+
+// app.get("/email", function(req, res){
+//
+//   var name = req.query.name,
+//     email = req.query.email,
+//     phone = req.query.phone,
+//     message = req.query.message;
+//  
+//   var msg = "Message received from contact me form: \n  Name: " + name + "\n  Email: " +email + "\n  Phone Number: " + phone + "\n  Message: " + message;
+//
+//   //console.log(req.query);
+//   //console.log(msg);
+//   
+//   exec("echo '" + msg + "' | mail -s 'Portfolio Query from " + req.body.name + "' benjamin.danen@gmail.com");
+//
+//   res.render("reload");
+// })
 
 app.listen(3000, function(){
     console.log("The server has started listening at port 3000");
